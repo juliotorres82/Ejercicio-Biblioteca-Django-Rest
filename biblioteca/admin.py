fe33696c326c08f49be1a983c1c61aec2178e39d
Julio <seiyasasu2001@gmail.com>
2025-09-17 10:12:40 -0600

    serializers terminados

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from biblioteca.models import Autor, Categoria, Libro, Prestamo
 
 # Register your models here.
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'nacionalidad', 'activo']
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ["titulo", "estado"]
+
+@admin.register(Prestamo)
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = ["usuario", "libro", "fecha_prestamo", "fecha_devolucion_esperada", "activo"]
