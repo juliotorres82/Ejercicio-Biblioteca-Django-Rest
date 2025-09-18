b5abb741bae74a6391d6544f32f9c71d8824c673
Julio <seiyasasu2001@gmail.com>
2025-09-18 17:38:52 -0600

    crud libros completado

--- biblioteca/api/routers.py
+++ biblioteca/api/routers.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from biblioteca.api.views import AutorListCreateView, AutorDetailView, CategoriaListCreateView, CategoriaDetailView
+from biblioteca.api.views import AutorListCreateView, AutorDetailView, CategoriaListCreateView, CategoriaDetailView, LibrosListCreateViev, LibrosDetailView
 
 urlpatterns = [
     path("api/autores/", AutorListCreateView.as_view(), name="autor-list-create"),
     path("api/autores/<int:id>/", AutorDetailView.as_view(), name="autor-detail"),
     path("api/categorias/", CategoriaListCreateView.as_view(), name="categoria-list-create"),
-    path("api/categorias/<int:id>/", CategoriaDetailView.as_view(), name="categoria-detail-view")
+    path("api/categorias/<int:id>/", CategoriaDetailView.as_view(), name="categoria-detail"),
+    path("api/libros/", LibrosListCreateViev.as_view(), name="libro-list-create"),
+    path("api/libros/<int:id>/", LibrosDetailView.as_view(), name="libro-detail")
+    
     
 
 ]
--- biblioteca/api/views.py
+++ biblioteca/api/views.py
@@ -129,4 +129,122 @@ class CategoriaDetailView(APIView):
         categoria.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
+    #Crud para libro
+    #Get y post en una sola vista
+class LibrosListCreateViev(APIView):
+    permission_classes = [permissions.AllowAny] # esto solo es para pruebas se cambiara en produccion a que solo admin pueda crear libros
+    def get(self, request):
+        #libro = Libro.objects.all()
+        libro = Libro.objects.select_related("autor", "categoria").order_by("id").all() # optimizar consultas con select_related para relaciones ForeignKey
+        # filtros por query params
+        # filtro por autor
+        autor = request.query_params.get("autor")
+        if autor:
+            q = Q(autor__nombre__iexact=autor) | Q(autor__apellido__iexact=autor)
+            if autor.isdigit():          # solo si el valor es numérico
+                q |= Q(autor__id=int(autor))
+            libro = libro.filter(q)
+
+        #filtro por categoria
+        categoria = request.query_params.get("categoria")
+        if categoria:
+            q = Q(categoria__nombre__iexact=categoria)
+            if categoria.isdigit():
+                q |= Q(categoria__id=int(categoria))
+            libro = libro.filter(q)
+        
+        #filtro por estado
+        estado = request.query_params.get("estado")
+        if estado:
+            libro = libro.filter(estado__iexact=estado)
+        
+        #filtro por precio
+        precio_min = request.query_params.get("precio_min")
+        precio_max = request.query_params.get("precio_max")
+        if precio_min:
+            try:
+                precio_min= float(precio_min)
+                libro = libro.filter(precio__gte=precio_min)
+            except ValueError:
+                pass
+
+        if precio_max:
+            try:
+                precio_max=float(precio_max)
+                libro = libro.filter(precio__lte=precio_max)
+            except ValueError:
+                pass
+        #filtro por año
+        anio = request.query_params.get("anio")
+        if anio:
+            libro = libro.filter(fecha_publicacion__year=anio)
+            
+        #filtro por busqueda
+        busqueda = request.query_params.get("busqueda")
+        if busqueda:
+            libro = libro.filter(
+                Q(autor__nombre__icontains=busqueda)|
+                Q(autor__apellido__icontains=busqueda)|
+                Q(categoria__nombre__icontains=busqueda)|
+                Q(estado__icontains=busqueda)|
+                Q(descripcion__icontains=busqueda)
+            )
+        #ordenamiento por multiples campos
+        ordering = request.query_params.get("ordering")
+        if ordering:
+            #soportar multiples campos al meterlos en una lista separados por coma
+            campos = [campo.strip() for campo in ordering.split(",") if campo.strip()]
+            if campos:
+                libro = libro.order_by(*campos)
+
+
+        serializer = LibroSerializer(libro, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def post(self, request):
+        serializer = LibroSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    #Get, put,patch,delete en una sola vista de un solo libro por ID
+class LibrosDetailView(APIView):
+    permission_classes = [permissions.AllowAny] # esto solo es para preba se cambiara en produccion a que solo admin pueda modificar libros
+    def get_object(self, id): #obtener un libro por id
+        try:
+            return Libro.objects.get(id=id)
+        except Libro.DoesNotExist:
+            return None
+    def get(self, request, id):
+        libro = self.get_object(id)
+        if not libro:
+            return Response({"error": "Libro no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = LibroSerializer(libro)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def put(self, request, id):
+        libro = self.get_object(id)
+        if not libro:
+            return Response({"error": "Libro no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = LibroSerializer(libro, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    def patch(self, request, id):
+        libro = self.get_object(id)
+        if not libro:
+            return Response({"error": "Libro no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = LibroSerializer(libro, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save() 
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    def delete(self, request, id):
+        libro = self.get_object(id)
+        if not libro:
+            return Response({"error":"Libro no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        libro.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)   
+
+
+
+
+    
 
